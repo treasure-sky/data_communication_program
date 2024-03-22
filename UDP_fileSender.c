1cc6e36b4f5e07fa25677ca9183b309a83df812b
treasure-sky <ych0486666@gmail.com>
2024-03-22 17:09:01 +0900

    feat: 파일 발신 기능 추가

--- UDP_fileSender.c
+++ UDP_fileSender.c
@@ -6,40 +6,61 @@
 #include <netinet/in.h> // 인터넷 주소 구조체
 #include <arpa/inet.h> // 인터넷 주소 변환 함수
 
-#define IP "192.168.0.1"
-#define PORT 1234
 #define GREETING "Greeting"
 #define OK "OK"
 
 int main(void){
-  struct sockaddr_in server_addr; // 수신자 정보 구조체
-  int sock, receive_len; // 소켓, 응답문자 개수
-  char receive_message[4];
-  
+  struct sockaddr_in receiver_addr; // 수신자 정보 구조체
+  int sock, receive_len, sock_len = sizeof(receiver_addr), packet_order = 0;
+  char receive_message[4], ip[16], file_name[256], file_ack[32];
+  unsigned short int port;
+
+  // ip, port 입력
+  printf("파일을 전송 받을 IP주소를 입력해주세요: ");
+  if(fgets(ip, sizeof(ip), stdin) == NULL){
+    perror("input IP format error");
+    exit(1);
+  }
+  ip[strcspn(ip, "\n")] = 0;
+
+  printf("파일을 전송 받을 PORT번호를 입력해주세요: ");
+  if(scanf("%hu", &port) == 0){
+    perror("input PORT format error");
+    exit(1);
+  }
+
+  // 파일 이름 입력
+  printf("파일의 이름을 입력해주세요(해당 프로그램과 같은 위치에 존재해야 합니다): ");
+  if(fgets(file_name, sizeof(file_name), stdin) == NULL){
+    perror("input file name format error");
+    exit(1);
+  }
+  file_name[strcspn(file_name, "\n")] = 0;
+
   // 소켓 생성
   if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
     perror("socket generate error");
     exit(1);
   }
 
-  memset(&server_addr, 0, sizeof(server_addr)); // 구조체 0으로 초기화
-  server_addr.sin_family = AF_INET; // IPv4로 설정
-  server_addr.sin_port = htons(PORT); // 포트를 bigEndian(네트워크 byte order)으로 변환 후 설정
+  memset(&receiver_addr, 0, sock_len); // 구조체 0으로 초기화
+  receiver_addr.sin_family = AF_INET; // IPv4로 설정
+  receiver_addr.sin_port = htons(port); // remote port를 bigEndian(네트워크 byte order)으로 변환 후 설정
 
-  // IP를 bigEndian(네트워크 byte order)으로 변환 후 설정
-  if (inet_aton(IP, &server_addr.sin_addr) == 0) { 
-    perror("IP format error");
+  // remote IP를 bigEndian(네트워크 byte order)으로 변환 후 설정
+  if (inet_aton(ip, &receiver_addr.sin_addr) == 0) { 
+    perror("inet_aton error");
     exit(1);
   }
 
   // Greeting 전송
-  if (sendto(sock, GREETING, strlen(GREETING), 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+  if (sendto(sock, GREETING, strlen(GREETING), 0, (struct sockaddr *)&receiver_addr, sock_len) == -1) {
     perror("greeting error");
     exit(1);
   }
 
   // OK 대기
-  if ((receive_len = recvfrom(sock, receive_message, strlen(receive_message), 0, (struct sockaddr *) &server_addr, sizeof(server_addr))) == -1) {
+  if ((receive_len = recvfrom(sock, receive_message, strlen(receive_message), 0, (struct sockaddr *) &receiver_addr, (socklen_t *) &sock_len)) == -1) {
     perror("ok error");
     exit(1);
   }
@@ -51,9 +72,52 @@ int main(void){
     exit(1);
   }
 
-  printf("OK");
+  // 파일 전송
+  FILE *file = fopen(file_name, "rb");
+  if(file == NULL) {
+    perror("file open error");
+    exit(1);
+  }
 
+  while (!feof(file))
+  {
+    char buffer[512]; // udp 패킷의 안전 범위
+    int next_bytes = fread(buffer, 1, sizeof(buffer), file);
+    if(next_bytes > 0) { // file에 내용이 아직 존재하면
+      char packet[516];
+      memcpy(packet, &packet_order, sizeof(int)); // packet 맨 앞에 패킷순서(packet_order)삽입
+      memcpy(packet + sizeof(int), buffer, next_bytes); // 데이터 삽입
+
+      while(1){
+        // 패킷 전송
+        if(sendto(sock, packet, next_bytes + sizeof(int), 0, (struct sockaddr *)&receiver_addr, sock_len) == -1){
+          perror("packet sending error");
+          exit(1);
+        }
+
+        // receiver 응답 대기
+        memset(file_ack, 0, sizeof(file_ack)); // 버퍼 초기화
+        if(recvfrom(sock, file_ack, sizeof(file_ack), 0, (struct sockaddr *) &receiver_addr, (socklen_t *) &sock_len) == -1){
+          perror("ack error");
+          exit(1);
+        }
+        
+        int now_order;
+        memcpy(&file_ack, &now_order, sizeof(int));
+        if(now_order != packet_order){
+          printf("not same packet");
+        } else{
+          break;
+        }
+      }
+      packet_order++;
+      
+    }
+  }
+  fclose(file);
   close(sock);
+  printf("file sended.");
   return 0;
 
 }
+
